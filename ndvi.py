7aaff4d4a33622b1d9097bb6d805c081363474a9
Joaquin Martinez <joaco1509@gmail.com>
2025-12-07 00:43:25 -0300

    changes in ndvi

--- ndvi.py
+++ ndvi.py
@@ -1,48 +1,46 @@
 import ee
-import requests
 import datetime
-import json
 
 ee.Authenticate()
 ee.Initialize(project="cellular-retina-276416")
 
-# 1. Load exact Uruguay boundary
-gaul = ee.FeatureCollection("FAO/GAUL/2015/level0")
-uruguay = gaul.filter(ee.Filter.eq("ADM0_NAME", "Uruguay")).geometry()
+def ndvi():
+    gaul = ee.FeatureCollection("FAO/GAUL/2015/level0")
+    uruguay = gaul.filter(ee.Filter.eq("ADM0_NAME", "Uruguay")).geometry()
 
-# Convert region to client-side GeoJSON
-uruguay_geojson = uruguay.getInfo()   # <--- fix!!
+    end = datetime.date.today()
+    start = end - datetime.timedelta(days=7)
 
-# 2. Date range
-end = datetime.date.today()
-start = end - datetime.timedelta(days=7)
+    col = (
+        ee.ImageCollection("MODIS/061/MOD09GA")
+        .filterBounds(uruguay)
+        .filterDate(str(start), str(end))
+        .sort("system:time_start", False)
+    )
 
-# 3. MODIS data
-col = (
-    ee.ImageCollection("MODIS/061/MOD09GA")
-    .filterBounds(uruguay)
-    .filterDate(str(start), str(end))
-    .sort("system:time_start", False)
-)
+    img = ee.Image(col.first())
 
-img = ee.Image(col.first())
+    ndvi = img.normalizedDifference(["sur_refl_b02", "sur_refl_b01"]).rename("NDVI")
+    ndvi = ndvi.clip(uruguay)
 
-# 4. NDVI
-ndvi = img.normalizedDifference(["sur_refl_b02", "sur_refl_b01"]).rename("NDVI")
-ndvi = ndvi.clip(uruguay)
+    today_str = datetime.datetime.now().strftime('%Y%m%d')
 
-# 5. Download
-url = ndvi.getDownloadURL({
-    "scale": 500,
-    "crs": "EPSG:4326",
-    "region": json.dumps(uruguay_geojson),   # <--- also fix
-    "format": "GEO_TIFF"
-})
+    file_name = f'ndvi/NDVI_Uruguay_{today_str}'
 
-output = "uruguay_modis_ndvi_exact.tif"
-r = requests.get(url)
+    task = ee.batch.Export.image.toCloudStorage(
+        image=ndvi,
+        description='NDVI_Uruguay_Export',
+        bucket='wildfires_data_um',            
+        fileNamePrefix=file_name,
+        region=uruguay.bounds(),
+        scale=500,
+        crs='EPSG:4326',
+        fileFormat='GeoTIFF',
+        maxPixels=1e13
+    )
 
-with open(output, "wb") as f:
-    f.write(r.content)
+    task.start()
+    print("Export started to GCS bucket 'wildfires_data_um'")
 
-print("Saved:", output)
+if __name__ == "__main__":
+    ndvi()
