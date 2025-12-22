823230f74581403ab5fbcf4efb0c084fe509ece7
Joaquin Martinez <joaco1509@gmail.com>
2025-12-22 00:16:02 -0300

    minor fix

--- uruguay_inference_pipeline.py
+++ uruguay_inference_pipeline.py
@@ -4,7 +4,7 @@ from uruguay_tiles import get_uruguay_tiles
 
 def inference_pipeline():
 
-    tiles_path=get_uruguay_tiles(max_tiles=50)
+    tiles_path=get_uruguay_tiles()
     
     inference(images_dir=tiles_path)
 
