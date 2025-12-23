e0f2720e2307f71118b9b4cc4a731912af21b707
Joaquin Martinez <joaco1509@gmail.com>
2025-12-23 00:49:19 -0300

    renamed scripts

---
+++ scripts/pipeline_metrics.py
@@ -0,0 +1,34 @@
+from metrics.download_aqua import export_modis_aqua_rgb
+from metrics.fwi import fwi
+from metrics.lst import download_modis_lst
+from metrics.ndvi import ndvi
+from utils import move_data_from_gcs_to_local
+import os
+
+if __name__ == "__main__":
+
+    gcs_paths = []
+
+    print("Starting data exports to GCS bucket...")
+    
+    fwi_path = fwi()
+    gcs_paths.append(fwi_path)
+    print("FWI exported to:", fwi_path)
+    
+    ndvi_path = ndvi()
+    gcs_paths.append(ndvi_path)
+    print("NDVI exported to:", ndvi_path)
+    
+    lst_path = download_modis_lst()
+    gcs_paths.append(lst_path)
+    print("LST exported to:", lst_path)
+    
+    aqua_rgb_path = export_modis_aqua_rgb()
+    gcs_paths.append(aqua_rgb_path)
+    print("MODIS AQUA RGB exported to:", aqua_rgb_path)
+    
+    print("All exports completed.")
+
+    local_dir = "data"
+    os.makedirs(local_dir, exist_ok=True)
+    move_data_from_gcs_to_local(gcs_paths, local_dir)
